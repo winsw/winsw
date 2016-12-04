6c0f6d1f7a94f24a68e5eb00563266c22cb2224b
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-12-04 09:34:19 +0100

    Fixes #142 - Deploy the automatic build on Appveyor (#144)
    * First configuration stub
    
    * Get rid of the old winsw_cert.pfx references, adjust docs
    
    * Fix the corrupted log4net reference, we use 2.0.3
    
    * Generate stub SNK file
    
    * Signing: Try full SDK path to generate SNKs
    
    * Fix the path
    
    * Signing: Sign all assemblies being packed into WinSW
    
    * Tests: Try enabling tests
    
    * Tests and artifacts: Use absolute paths
    
    * Artifact path must be relative
    
    * The test DLL is the NUnit one
    
    * nunit-console does not require loggers
    
    * NUnit: Try picking all DLLs in the output folder
    
    * NUnit console: No wildcards
    
    * Tests: Fix the test project to make it properly working with the new project structure
    
    * Docs: Clarify the specifics of external extension usage
    
    * Add AppVeyor badge to README.md

--- sign.sh
+++ sign.sh
@@ -2,7 +2,7 @@
 export "PATH=$PATH:/cygdrive/c/Program Files/Windows Kits/8.1/bin/x86"
 for f in Release Debug;
 do
-  signtool sign /f winsw_cert.pfx /t http://timestamp.verisign.com/scripts/timestamp.dll bin/$f/winsw.exe
+  signtool sign /f winsw-key.snk /t http://timestamp.verisign.com/scripts/timestamp.dll bin/$f/winsw.exe
   signtool verify /v /pa bin/$f/winsw.exe
 done
 echo success
---
+++ src/Test/winswTests/Extensions/ExtensionTestBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace winswTests.Extensions
+{
+    /// <summary>
+    /// Base class for testing of WinSW Extensions.
+    /// </summary>
+    public class ExtensionTestBase
+    {
+        /// <summary>
+        /// Defines the name of the extension to be passed in the configuration.
+        /// This name should point to assembly in tests, because we do not merge extension DLLs for testing purposes.
+        /// </summary>
+        /// <param name="type">Type of the extension</param>
+        /// <returns>String for Type locator, which includes class and assembly names</returns>
+        protected static String getExtensionClassNameWithAssembly(Type type)
+        {
+            return type.ToString() + ", " + type.Assembly;
+        }
+    }
+}
