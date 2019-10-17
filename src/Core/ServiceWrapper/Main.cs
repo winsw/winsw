a8fee1975f0787aa44d9c42de3e4f8a4d44b6394
Michael Karmazin <michael.karmazin@cba.com.au>
2019-10-17 11:21:08 +1100

    Add stopwait command

--- src/Core/ServiceWrapper/Main.cs
+++ src/Core/ServiceWrapper/Main.cs
@@ -607,6 +607,10 @@ namespace winsw
                     Stop();
                     return;
 
+                case "stopwait":
+                    StopWait();
+                    return;
+
                 case "restart":
                     Restart();
                     return;
@@ -815,6 +819,25 @@ namespace winsw
                 }
             }
 
+            void StopWait()
+            {
+                Log.Info("Stopping the service with id '" + descriptor.Id + "'");
+                if (s is null)
+                    ThrowNoSuchService();
+
+                if (s.Started)
+                    s.StopService();
+
+                while (s != null && s.Started)
+                {
+                    Log.Info("Waiting the service to stop...");
+                    Thread.Sleep(1000);
+                    s = svc.Select(descriptor.Id);
+                }
+
+                Log.Info("The service stopped.");
+            }
+
             void Restart()
             {
                 Log.Info("Restarting the service with id '" + descriptor.Id + "'");
@@ -984,6 +1007,7 @@ namespace winsw
   uninstall   uninstall the service
   start       start the service (must be installed before)
   stop        stop the service
+  stopwait    stop the service and wait until it's actually stopped
   restart     restart the service
   restart!    self-restart (can be called from child processes)
   status      check the current status of the service
