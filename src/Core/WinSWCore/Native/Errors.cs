10f8ab5e54cb7ae3ff744c7417123eeefff1aba0
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-03-16 00:00:00 +0800

    Improve signal-related logging

--- src/Core/WinSWCore/Native/Errors.cs
+++ src/Core/WinSWCore/Native/Errors.cs
@@ -2,6 +2,9 @@
 {
     internal static class Errors
     {
+        internal const int ERROR_ACCESS_DENIED = 5;
+        internal const int ERROR_INVALID_HANDLE = 6;
+        internal const int ERROR_INVALID_PARAMETER = 7;
         internal const int ERROR_CANCELLED = 1223;
     }
 }
