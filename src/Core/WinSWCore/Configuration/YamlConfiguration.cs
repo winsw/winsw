24131906aca9bb226f046b2149d59c5066aabe7c
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-06-02 14:47:33 +0530

    Add YamlConfigurations.cs YmlDotNet library added

---
+++ src/Core/WinSWCore/Configuration/YamlConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace winsw.Configuration
+{
+    public class YamlConfiguration
+    {
+        readonly ServiceAccount? serviceAccount;
+        readonly BasicConfigs? basicConfigs;
+    }
+
+    public class ServiceAccount
+    {
+        public string name { get; set; }
+        public string domain { get; set; }
+        public string user { get; set; }
+        public string allowservicelogon { get; set; }
+    }
+
+    public class BasicConfigs
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string executable { get; set; }
+    }
+}
