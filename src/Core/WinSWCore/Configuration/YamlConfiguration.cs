f4fe7bd97fd1fe32c6b4699339970df73a400aa9
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-06-04 02:28:53 +0530

    Add  ServiceDescriptrYaml unit test Update YamlConfigurations.

--- src/Core/WinSWCore/Configuration/YamlConfiguration.cs
+++ src/Core/WinSWCore/Configuration/YamlConfiguration.cs
@@ -1,32 +1,48 @@
-﻿namespace winsw.Configuration
+﻿using System.Collections.Generic;
+
+namespace winsw.Configuration
 {
     public class YamlConfiguration
     {
-        public string? id { get; set; }
-        public string? name { get; set; }
-        public string? description { get; set; }
-        public string? executable { get; set; }
-        public string? workingdirectory { get; set; }
+        public string? id;
+        public string? name;
+        public string? description;
+        public string? executable;
+        public string? workingdirectory;
 
-        public ServiceAccount? serviceaccount { get; set; }
-        public Log? log { get; set; }
+        public ServiceAccount? serviceaccount;
+        public Log? log;
+
+        public List<Downloading>? download;
     }
 
     public class ServiceAccount
     {
-        public string name { get; set; }
-        public string domain { get; set; }
-        public string user { get; set; }
-        public string allowservicelogon { get; set; }
+        public string name;
+        public string domain;
+        public string user;
+        public string allowservicelogon;
     }
 
     public class Log
     {
-        public string sizeThreshold { get; set; }
-        public string keepFiles { get; set; }
-        public string pattern { get; set; }
-        public string autoRollAtTime { get; set; }
-        public string period { get; set; }
-        public string mod { get; set; }
+        public string? sizeThreshold;
+        public string? keepFiles;
+        public string? pattern;
+        public string? autoRollAtTime;
+        public string? period;
+        public string? mod;
+    }
+
+    public class Downloading
+    {
+        public string from;
+        public string to;
+        public string auth;
+        public string? username;
+        public string? password;
+        public bool unsecureAuth;
+        public bool failOnError;
+        public string? proxy;
     }
 }
