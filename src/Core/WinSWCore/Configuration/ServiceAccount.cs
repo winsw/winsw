d687fece6548b2b5c515a1bd7bafc15c38036ec1
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-06-24 01:34:04 +0530

    Update Yaml Confifurations

--- src/Core/WinSWCore/Configuration/ServiceAccount.cs
+++ src/Core/WinSWCore/Configuration/ServiceAccount.cs
@@ -1,11 +1,22 @@
-﻿namespace winsw.Configuration
+﻿using YamlDotNet.Serialization;
+
+namespace winsw.Configuration
 {
     public class ServiceAccount
     {
-        public string? name { get; set; }
-        public string? domain;
-        public string? user;
-        public string? password { get; set; }
-        public string? allowservicelogon;
+        [YamlMember(Alias = "name")]
+        public string? Name { get; set; }
+
+        [YamlMember(Alias = "domain")]
+        public string? Domain { get; set; }
+
+        [YamlMember(Alias = "user")]
+        public string? User{ get; set; }
+
+        [YamlMember(Alias = "Password")]
+        public string? Password { get; set; }
+
+        [YamlMember(Alias = "allowservicelogon")]
+        public string? AllowServiceLogon { get; set; }
     }
 }
