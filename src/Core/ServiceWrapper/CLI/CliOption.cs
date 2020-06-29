281f419df0decdb761d6e95f2a7915090a1c2141
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-06-29 22:32:45 +0530

    Add short option names to CLI

--- src/Core/ServiceWrapper/CLI/CliOption.cs
+++ src/Core/ServiceWrapper/CLI/CliOption.cs
@@ -4,16 +4,16 @@ namespace winsw.CLI
 {
     public class CliOption
     {
-        [Option("configFile", HelpText = "Configurations File")]
-        public string ConfigFile { get; set; }
+        [Option('c', "configFile", HelpText = "Configurations File")]
+        public string? ConfigFile { get; set; }
 
-        [Option("elevated", HelpText = "Elevated Command Prompt", Default = false)]
+        [Option('e', "elevated", HelpText = "Elevated Command Prompt", Default = false)]
         public bool Elevate { get; set; }
 
-        [Option("redirect", HelpText = "Redirect Logs")]
-        public string RedirectPath { get; set; }
+        [Option('r', "redirect", HelpText = "Redirect Logs")]
+        public string? RedirectPath { get; set; }
 
-        [Option("skipConfigValidation", HelpText = "Enable configurations schema validation", Default = false)]
+        [Option('s', "skipConfigValidation", HelpText = "Enable configurations schema validation", Default = false)]
         public bool validation { get; set; }
     }
 }
