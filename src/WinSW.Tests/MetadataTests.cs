cb46626299fc3182434a7e84a4230ea5b346a3f7
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-08-06 00:00:00 +0800

    Update .NET Framework packaging

---
+++ src/WinSW.Tests/MetadataTests.cs
@@ -0,0 +1,32 @@
+﻿#if NET461
+using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using WinSW.Tests.Util;
+using Xunit;
+
+namespace WinSW.Tests
+{
+    public sealed class MetadataTests
+    {
+        [Fact]
+        public void Extern()
+        {
+            var version = new Version(4, 0, 0, 0);
+
+            using var file = File.OpenRead(Path.Combine(Layout.ArtifactsDirectory, "WinSW.NET461.exe"));
+            using var peReader = new PEReader(file);
+            var metadataReader = peReader.GetMetadataReader();
+            foreach (var handle in metadataReader.AssemblyReferences)
+            {
+                var assembly = metadataReader.GetAssemblyReference(handle);
+                if (metadataReader.GetString(assembly.Name) != "System.IO.Compression")
+                {
+                    Assert.Equal(version, assembly.Version);
+                }
+            }
+        }
+    }
+}
+#endif
