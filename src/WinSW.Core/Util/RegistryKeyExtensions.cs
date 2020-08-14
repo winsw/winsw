2fc786a26aaef5809dd5aa88b7e71681b5473117
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-08-15 00:00:00 +0800

    Auto refresh

---
+++ src/WinSW.Core/Util/RegistryKeyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Win32;
+using WinSW.Native;
+using static WinSW.Native.RegistryApis;
+
+namespace WinSW.Util
+{
+    internal static class RegistryKeyExtensions
+    {
+        /// <exception cref="CommandException" />
+        internal static unsafe DateTime GetLastWriteTime(this RegistryKey registryKey)
+        {
+            int error = RegQueryInfoKeyW(registryKey.Handle, null, null, null, null, null, null, null, null, null, null, out FILETIME lastWriteTime);
+            if (error != Errors.ERROR_SUCCESS)
+            {
+                Throw.Command.Win32Exception(error, "Failed to query registry key.");
+            }
+
+            return DateTime.FromFileTime(lastWriteTime.ToTicks());
+        }
+    }
+}
