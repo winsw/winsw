ea2bff585e10cc142159adda88cddfae113d0fc5
Sebastian Bäumlisberger <s.baeumlisberger@live.de>
2021-11-01 10:00:00 +0100

    trim whitespace

--- src/WinSW.Core/Configuration/XmlServiceConfig.cs
+++ src/WinSW.Core/Configuration/XmlServiceConfig.cs
@@ -654,8 +654,8 @@ namespace WinSW
                     throw new WinSWException("The environment variables file contains one or more invalid entries. Each variable definition must be on a separate line and in the format \"key=value\".");
                 }
 
-                string key = line.Substring(0, equalsSignIndex);
-                string value = line.Substring(equalsSignIndex + 1);
+                string key = line.Substring(0, equalsSignIndex).Trim();
+                string value = line.Substring(equalsSignIndex + 1).Trim();
 
                 Environment.SetEnvironmentVariable(key, value);
             }
